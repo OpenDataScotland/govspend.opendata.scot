ae8ac83a810a5e1e90a4878c4fbc7f136a9b5f9f
Jack Gilmore <46202639+JackGilmore@users.noreply.github.com>
2023-08-09 19:48:05 +0000

    Restructure data

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,23 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
 
-    eleventyConfig.addCollection("spendsOver500", (collection) => {
-      const allItems = collection.getAll()[0].data.spendover500;
+  eleventyConfig.addCollection("spendsOver500", (collection) => {
+    const allItems = collection.getAll()[0].data.spendover500;
+
+    var spendCollection = [];
+
+    for (const [key, value] of Object.entries(allItems)) {
+      var monthSpendData = {
+        "title": key,
+        "data": value
+      }
+
+      spendCollection.push(monthSpendData);
+    }
     
-      // Filter or use another method to select the items you want
-      // for the collection
-      return allItems;
-    });
+    return spendCollection;
+  });
 
-    eleventyConfig.addFilter("jsonify", function(value) { return JSON.stringify(value, null, 4) });
+  eleventyConfig.addFilter("jsonify", function (value) { return JSON.stringify(value, null, 4) });
 
-  };
-  
+};
