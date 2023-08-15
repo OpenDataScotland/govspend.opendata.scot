516a10cba6ee8515f91fae731f21fa969747cec7
Jack Gilmore <46202639+JackGilmore@users.noreply.github.com>
2023-08-15 19:39:54 +0000

    Sitemap.xml and robots.txt

--- _data/site.js
+++ _data/site.js
@@ -2,4 +2,6 @@ module.exports = {
 
     // Outputs a unix timestamp that can be used to cache bust our files
     buildTime: Date.now(),
+    name: "ODS GovSpend",
+    url: "https://govspend.opendata.scot"
   };
